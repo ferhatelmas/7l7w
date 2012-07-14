74ec388c3d921a12731d1882e63b6d54877cb638
ferhat elmas <elmas.ferhat@gmail.com>
2012-07-15 00:08:14 +0200

    refactored tiny-grep

--- ruby/2/grep.rb
+++ ruby/2/grep.rb
@@ -1,12 +1,12 @@
 if ARGV.length < 2
-	puts "Usage ruby grep.rb <filename> <regex>"
-end
+	puts "Usage: ruby grep.rb <filename> <regex>"
+else 
+	file = File.open ARGV[0]
+	pattern = Regexp.new ARGV[1]
 
-file = File.open ARGV[0]
-pattern = Regexp.new ARGV[1]
-
-lc = 1
-file.each do |line|
-	puts "#{lc}: #{line}" if pattern.match line
-	lc = lc + 1
+	lc = 1
+	file.each do |line|
+		puts "#{lc}: #{line}" if pattern.match line
+		lc = lc + 1
+	end
 end
