ceea94cd87726b7187af26ce227a08d913e484d8
ferhat elmas <elmas.ferhat@gmail.com>
2012-07-26 00:49:59 +0200

    added prolog day 1

---
+++ prolog/1/plays.pl
@@ -0,0 +1,10 @@
+plays(a, guitar).
+plays(b, guitar).
+plays(c, piano).
+
+plays_in(a, pop).
+plays_in(b, pop).
+plays_in(b, rock).
+plays_in(c, classical).
+
+plays(What, guitar).
---
+++ prolog/1/writes.pl
@@ -0,0 +1,10 @@
+writes(ferdinand_von_schirach, schuld).
+writes(grimm, household_stories).
+writes(mario_mazzanti, checkmate).
+writes(strunk, elements_of_style).
+writes(henney, ninety_seven_things_every_programmer_should_know).
+writes(hunt_thomas, pragmatic_programmer).
+writes(fowler, passionate_programmer).
+writes(tate, seven_languages_in_seven_weeks).
+
+writes(tate, What).
